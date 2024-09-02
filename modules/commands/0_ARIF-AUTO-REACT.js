0cec82909bd80dc4ee4bbcafcafa1b92dc6bc067
GITHUB78600 <githup3@yandex.com>
2024-09-02 08:56:05 +0530

    Update and rename autoreact.js to 0_ARIF-AUTO-REACT.js

--- modules/commands/0_ARIF-AUTO-REACT.js
+++ modules/commands/0_ARIF-AUTO-REACT.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autoreact",
 	version: "1.1.1",
 	hasPermission: 0,
-	credits: "AYAN",
+	credits: "ARIF BABU",
 	description: "Bot React",
 	commandCategory: "No Prefix",
 	cooldowns: 0,
